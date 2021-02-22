dfe98bda49e85fe9006da5de64d285968d009cb8
LVYUEDONG <1621259898@qq.com>
2021-02-22 13:17:07 +0800

    修复BUG

--- src/config.php
+++ src/config.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'success_code'    => 1, //成功返回的code值
-    'error_code'      => 0, //失败返回的code值
-    'base_controller' => 'Right', //控制器基类，为空则不继承任何类
-    'sign_controller' => 'SignIn', //带登录验证的控制器基类,为空则使用base_controller的值
-    'model_path'      => '', //模型相对路径，从base_path开始，为空则使用默认（多应用为common/model，但应用为model）
-    'index_template'  => '', //列表页模板，为空则使用默认
-    'add_template'    => '', //添加页模板，为空则使用默认
-    'edit_template'   => '', //修改页模板，为空则使用默认
+    'success_code' => 1, //成功返回的code值
+    'error_code' => 0, //失败返回的code值
+    'back_base_controller' => 'Right', //后台控制器基类，为空则使用\think\Controller
+    'front_base_controller' => '', //前台无需登录的控制器基类,为空则使用\think\Controller
+    'front_sign_controller' => 'SignIn', //前台带登录验证的控制器基类,为空则使用front_base_controller的值
+    'index_template' => '', //列表页模板，为空则使用默认
+    'add_template' => '', //添加页模板，为空则使用默认
+    'edit_template' => '', //修改页模板，为空则使用默认
     /*
      * form表单字段模板，指定使用以下占位符
      * {{name}}{{label}}{{value}}{{attr}}
@@ -34,5 +34,9 @@ return [
         'date'     => '<l-date v-model="formData.{{name}}" label="{{label}}"></l-date>',
         'datetime' => '<l-date-time v-model="formData.{{name}}" label="{{label}}"></l-date-time>',
         'textarea' => '<v-textarea v-model="formData.{{name}}" label="{{label}}" solo rows="3" auto-grow></v-textarea>',
+    ],
+    'yapi' => [
+        'domain' => '', //yapi域名
+        'token' => '', //项目token,
     ],
 ];
